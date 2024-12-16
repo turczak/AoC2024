c1db425c39d5aac3cfff94178f1e40a97b65ccff
turczak <wojciech.turczak97@gmail.com>
2024-12-16 16:29:14 +0100

    Day 1, part II

--- src/main/java/day1/Day1.java
+++ src/main/java/day1/Day1.java
@@ -2,26 +2,25 @@ package day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day1 {
     private List<Integer> leftList;
     private List<Integer> rightList;
     private final File file = new File("src/main/resources/day1.txt");
     private Scanner scanner;
+    private HashMap<Integer, Integer> map;
 
     public Day1() throws FileNotFoundException {
         this.scanner = new Scanner(file);
         this.leftList = new ArrayList<>();
         this.rightList = new ArrayList<>();
         getInput();
+        this.map = new HashMap<>();
     }
 
-    private void getInput(){
-        while (scanner.hasNextLine()){
+    private void getInput() {
+        while (scanner.hasNextLine()) {
             String[] line = scanner.nextLine().split("   ");
             Integer left = Integer.parseInt(line[0]);
             Integer right = Integer.parseInt(line[1]);
@@ -51,5 +50,26 @@ public class Day1 {
 
     public Integer getAnswerA() {
         return calculateTotalDistance(leftList, rightList);
+    }
+
+    private void listToMap() {
+        for (Integer number : rightList) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+    }
+
+    private Integer calculateSimilarityScore() {
+        Integer score = 0;
+
+        for (Integer number : leftList) {
+            Integer frequency = map.getOrDefault(number, 0);
+            score += number * frequency;
+        }
+
+        return score;
+    }
+
+    public Integer getAnswerB() {
+        return calculateSimilarityScore();
     }
 }
